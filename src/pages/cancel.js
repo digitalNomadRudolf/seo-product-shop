1174750329756125966fef8bed8a71097949ebd8
rudolf <rudolf@example.com>
2019-10-03 23:12:13 +0100

    some updates

--- src/pages/cancel.js
+++ src/pages/cancel.js
@@ -9,7 +9,11 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>The transaction has been canceled.</h1>
     <p className="cancel-p">You can always come back and purchase when ready.</p>
+
+    <div className="button-wrap">
     <Link to="/"><span className="backhome">Go back to the homepage</span></Link>
+    <Link className="toproducts" to="/products">More products</Link>
+    </div>
   </Layout>
 )
 
--- src/pages/success.js
+++ src/pages/success.js
@@ -9,7 +9,11 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Congratulations!</h1>
     <p className="success-p">You have completed the first step to attracting highly converting traffic to your website.</p>
+
+    <div className="button-wrap">
     <Link to="/"><span className="backhome">Go back to the homepage</span></Link>
+    <Link className="toproducts" to="/products">More products</Link>
+    </div>
   </Layout>
 )
 

c1f1c81fdef8b062f1e7df906232a30c712eec1f
digitalNomadRudolf <46172758+digitalNomadRudolf@users.noreply.github.com>
2019-10-04 11:24:17 +0100

    Create ProductLayout.js

---
+++ src/components/ProductLayout.js
@@ -0,0 +1,57 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
+
+import Header from "./header";
+import './layout.scss';
+//import "./layout.css"
+
+const ProductLayout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query OtherTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div className="homecontent__wrap"
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+        }}
+      >
+        
+    
+        <main className={`maincontent takeflex`}>{children}</main>
+        {/* <footer className="footer">
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer> */}
+        
+      </div>
+    </>
+  )
+}
+
+ProductLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default ProductLayout
+
